54680e3dd3a201d18547464f805643c9fb067c8c
JadeLeng <showus2006@foxmail.com>
2017-11-02 23:39:00 -0500

    [Moded]backserver new node

--- backserver.go
+++ backserver.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	rpc.Register(shareReadWrite.NewNode("localhost:9876", "localhost:10030"))
+	rpc.HandleHTTP()
 	l, err := net.Listen("tcp", ":9876")
 	if err != nil {
 		log.Fatal("listen error:", err)
